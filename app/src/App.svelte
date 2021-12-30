5180dfde5dad6ebea8ec4269c6ef151c91563972
Thales Rodrigues <thales.brito@hotmail.com>
2021-12-29 21:32:29 -0300

    Svelte mock app

---
+++ app/src/App.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	let connected:boolean = false;
+
+	let ws:WebSocket;
+
+	function connect() {
+		ws = new WebSocket("ws://localhost:8080");
+
+		ws.onopen = () => {
+			console.log("Connection open");
+			connected = true;
+		}
+
+		ws.onclose = () => {
+			console.log("Connection lost");
+			connected = false;
+		}
+	}
+
+	function disconnect() {
+		ws.close();
+	}
+</script>
+
+<main>
+	<h1>SharedIO Manual Test</h1>
+	<p>Current status: {connected ? "Connected" : "Disconnected"}</p>
+	{#if !connected}
+		<button on:click={connect}>Connect</button>
+	{:else}
+		<button on:click={disconnect}>Disconnect</button>
+	{/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
